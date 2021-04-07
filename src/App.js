d2bdd4fa1f92fe7c063bbee41f40da0b7f335140
saiful29 <shaifulbd123@gmail.com>
2021-04-07 10:46:51 +0600

    resteart project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {InputGroup, FormControl} from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <div className="d-flex justify-content-center align-items-center" style={{minHeight: '100vh'}}>
+         <div className="border shadow bg-light p-5">
+           <div className="h3 font-weight-bold border-bottom mb-4"> Login </div>
+           <div className="mb-4">
+             <InputGroup className="mb-3">
+               <InputGroup.Prepend>
+                 <InputGroup.Text id="basic-addon1"> <FontAwesomeIcon icon="user" /></InputGroup.Text>
+               </InputGroup.Prepend>
+               <FormControl
+                   placeholder="Username"
+                   aria-label="Username"
+                   aria-describedby="basic-addon1"
+               />
+             </InputGroup>
+           </div>
+           <div className="mb-4">
+             <InputGroup className="mb-3">
+               <InputGroup.Prepend>
+                 <InputGroup.Text id="basic-addon2"><FontAwesomeIcon icon="key" /></InputGroup.Text>
+               </InputGroup.Prepend>
+               <FormControl
+                   placeholder="Password"
+                   aria-label="Password"
+                   aria-describedby="basic-addon2"
+               />
+             </InputGroup>
+           </div>
+           <div className="d-flex justify-content-center">
+             <button className="btn btn-outline-info"> Login </button>
+           </div>
+         </div>
+        </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './FontAwesome';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './Scss/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
